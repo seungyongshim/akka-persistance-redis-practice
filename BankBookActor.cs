0eb775768d306d4ec5cb81ed8eee7981a9278941
심승용 <seungyong.shim@mirero.co.kr>
2020-08-15 14:11:25 +0900

    no message

---
+++ BankBookActor.cs
@@ -0,0 +1,36 @@
+﻿using Akka.Actor;
+using Akka.Persistence;
+using System;
+
+namespace Akka_Persistence_Sqlite
+{
+    public class BankBookActor : ReceivePersistentActor
+    {
+        public BankBookActor(string id)
+        {
+            PersistenceId = id;
+
+            Command<int>(CommandHandle);
+            Recover<int>(RecoverHandle);
+        }
+
+        public override string PersistenceId { get; }
+
+        public int TotalAmount { get; set; } = 0;
+
+        public static Props Props(string id) =>
+                            Akka.Actor.Props.Create(() => new BankBookActor(id));
+
+        private void CommandHandle(int m)
+        {
+            TotalAmount += m;
+            Persist(m, _ => { });
+            Console.WriteLine($"TotalAmount:{TotalAmount}");
+        }
+
+        private void RecoverHandle(int m)
+        {
+            TotalAmount += m;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Persistence.Redis;
+using Akka.Util.Internal;
+
+namespace Akka_Persistence_Sqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = CreateConfig(Assembly.GetExecutingAssembly());
+
+            using (var system = ActorSystem.Create("example", config.WithFallback(ConfigurationFactory.Default())))
+            {
+                RedisPersistence.Get(system);
+                var bankBookActor = system.ActorOf(BankBookActor.Props("1"));
+
+                bankBookActor.Tell(100);
+                bankBookActor.Tell(-50);
+
+                Console.ReadLine();
+            }
+
+
+        }
+
+        private static Config CreateConfig(Assembly assembly)
+        {
+            var assemblyFilePath = new Uri(assembly.GetName().CodeBase).LocalPath;
+            var assemblyDirectoryPath = Path.GetDirectoryName(assemblyFilePath);
+            var hoconFileName = "config";
+            var hoconFilePath = $@"{assemblyDirectoryPath}{Path.DirectorySeparatorChar}{hoconFileName}.hocon";
+
+            return ConfigurationFactory.ParseString(File.ReadAllText(hoconFilePath));
+        }
+    }
+}
